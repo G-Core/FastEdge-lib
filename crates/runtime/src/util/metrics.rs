6987aa8bceae14729dd0b13ac3f7ef1512e762ac
Ruslan Pislari <ruslan.pislari@gcore.com>
2026-02-20 10:15:15 +0200

    fix: change duration buckets to fastedge request duration metrics

--- crates/runtime/src/util/metrics.rs
+++ crates/runtime/src/util/metrics.rs
@@ -21,7 +21,11 @@ lazy_static! {
     static ref REQUEST_DURATION: HistogramVec = register_histogram_vec!(
         "fastedge_request_duration",
         "Request duration",
-        &["executor"]
+        &["executor"],
+        vec![
+            0.0001, 0.0002, 0.0003, 0.0005, 0.001, 0.002, 0.003, 0.005, 0.01, 0.02, 0.03, 0.05,
+            0.1, 0.2, 0.3, 0.5, 1.0, 10.0
+        ]
     )
     .unwrap();
     static ref MEMORY_USAGE: IntCounterVec = register_int_counter_vec!(
